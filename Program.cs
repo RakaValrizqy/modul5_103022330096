851db84dc770cae707c52b6b092e03be0045779b
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-19 16:46:56 +0700

    fix input

--- Program.cs
+++ Program.cs
@@ -29,10 +29,13 @@ class Program
 {
     static void Main(string[] args)
     {
+        int x = 10;
+        int y = 30;
+        int z = 22;
         SimpleDataBase<int> db = new SimpleDataBase<int>();
-        db.AddNewData(12);
-        db.AddNewData(34);
-        db.AddNewData(56);
+        db.AddNewData(x);
+        db.AddNewData(y);
+        db.AddNewData(z);
         db.PrintAllData();
     }
 }
